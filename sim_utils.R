e52d3d5674f896dfe39c44e581b135be9e722066
Matei Ionita <matei.ionita@gmail.com>
2020-09-06 16:01:55 -0400

    added code to simulate data

---
+++ sim_utils.R
@@ -0,0 +1,54 @@
+generate_cluster_phenos <- function(K_true, P) {
+  alpha <- rep(0.5, P)
+  mat <- DirichletReg::rdirichlet(2 * K_true, alpha)
+  comp <- matrix(vapply(mat, function(v) ifelse(v > 1.6/P, 2L, 1L), integer(1)), nrow = 2 * K_true)
+  comp <- choose_unique_phenos(comp, K_true - 1)
+  comp <- rbind(rep(1L,P) , comp)
+  return(comp)
+}
+
+choose_unique_phenos <- function(comp, n_unique) {
+  phenos <- apply(comp, 1, function(x) paste(x, collapse = ""))
+  unique_pheno_list <- split(seq_along(phenos), phenos)
+  unique_indices <- as.integer(vapply(unique_pheno_list, function(x) x[1], integer(1)))
+  # sel <- sample(unique_indices,n_unique)
+  sel <- unique_indices[seq(n_unique)]
+  
+  return(comp[sel,])
+}
+
+count_pheno <- function(comp) {
+  phenos <- apply(comp, 1, function(x) paste(x, collapse = ""))
+  return(length(unique(phenos)))
+}
+
+get_sd_marginal <- function(P) {
+  col <- runif(P, min = 0.6, max = 0.95)
+  pros_marginal <- cbind(col, 1-col)
+  sd_marginal <- 1/sqrt(pros_marginal) * mean(sqrt(pros_marginal)) * 0.5
+  
+  return(sd_marginal)
+}
+
+
+plot_kdes_with_means <- function(data, means) {
+  plot_list <- list()
+  P <- ncol(data)
+  
+  for (p in seq(P)) {
+    kde <- KernSmooth::bkde(data[,p])
+    df_kde <- data.frame(x = kde$x, y = kde$y)
+    df_means <- data.frame(m = as.vector(means[,,p]))
+    
+    g <- ggplot() +
+      geom_line(data = df_kde, mapping = aes(x=x, y=y)) +
+      geom_point(data = df_means, mapping = aes(x=m, y=0), size = 2, alpha = 0.3) +
+      xlim(-1,5) +
+      labs(title = names[p], x = "", y = "") +
+      theme_bw()
+    
+    plot_list[[p]] <- g
+  }
+  
+  return(gridExtra::grid.arrange(grobs = plot_list, ncol = 5))
+}
---
+++ simulate_data.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+
+# Set some constants
+K_true <- 75L # clusters
+P <- 10L # markers
+M <- 10L # samples
+N <- 3e5L # events per sample
+set.seed(1258) # reproducibility
+
+# Generate phenotypes. Optional, tabulate number of positive markers
+comp <- generate_cluster_phenos(K_true, P)
+# count_positive <- apply(comp, 1, function(v) length(which(v == 2)))
+# table(count_positive)
+
+# Generate mixture proportions
+pro_baseline <- rbeta(K_true, shape1 = 2, shape2 = 8) # baseline proportions
+pro_by_sample <- seq(M) %>%
+  lapply(function(x) pro_baseline * runif(K_true, min = 0.7, max = 1.3)) %>%
+  lapply(function(x) x / sum(x)) %>%
+  do.call(what = rbind) # add some random variation across samples
+
+# Generate means
+means_baseline <- cbind(rep(1.1,P) , rep(3,P))
+sd_marginal <- get_sd_marginal(P) # legacy function; should be replaced with runif(2*P,0.05,0.2)
+
+means_by_sample <- array(dim = c(M, K_true,P))
+for (m in seq(M)) {
+  for (p in seq(P)) {
+    means_by_sample[m,,p] <- vapply(comp[,p], function(x) rnorm(1, mean = means_baseline[p,x], sd = sd_marginal[p,x]/6),
+                               numeric(1))
+  }
+}
+
+# Generate variances: for simplicity, the same for all samples
+var_baseline <- array(dim = c(P,P,K_true))
+for (k in seq_len(K_true)) {
+  c <- comp[k,]
+  d <- vapply(c(1:P), function(x) sd_marginal[x,c[x]], numeric(1))
+  var_baseline[,,k] <- rWishart(1, df = P, Sigma = diag(d) / P)
+}
+
+# Generate data
+data <- matrix(nrow = N * M, ncol = P)
+names <- vapply(c(1:P), function(x) paste("M", x, sep = ""), character(1))
+colnames(data) <- names
+colnames(comp) <- names
+true_labels <- integer(N * M)
+
+start <- 1
+for (m in seq(M)) {
+  for (k in seq(K_true)) {
+    # How many events drawn from this cluster?
+    this_N <- ifelse(k == K_true, m*N - start + 1, floor(pro_by_sample[m,k] * N))
+    stop <- start + this_N - 1
+    
+    data[c(start:stop),] <- mvtnorm::rmvnorm(this_N, mean = means_by_sample[m,k,], 
+                                             sigma = var_baseline[,,k])
+    true_labels[c(start:stop)] <- k
+    
+    start <- stop + 1
+  }
+}
+
+# Inspect data
+suppressWarnings( plot_kdes_with_means(data, means_by_sample) )
+
+
+
+
+
